7ccedbc29d1df9b85c80becbf8393f0c7a8bef87
Lucy Moss <thecoloryes@gmail.com>
2020-02-18 20:24:22 -0500

    fixed player class

--- carcassonne/scorer/models.py
+++ carcassonne/scorer/models.py
@@ -3,7 +3,7 @@ from django.db import models
 RIVER_TILE_COUNT = 12
 BASE_TILE_COUNT = 72
 
-class Player(models.Model):
+class Player(models.User):
     icon = models.ImageField()
     wins = models.IntegerField(default=0)
     losses = models.IntegerField(default=0) 
