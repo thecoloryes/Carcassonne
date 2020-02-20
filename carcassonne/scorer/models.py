00d552d6e7dff87008cdc5cdafebfad284797387
Lucy Moss <thecoloryes@gmail.com>
2020-02-19 19:29:48 -0500

    fixed models

--- carcassonne/scorer/models.py
+++ carcassonne/scorer/models.py
@@ -5,7 +5,7 @@ RIVER_TILE_COUNT = 12
 BASE_TILE_COUNT = 72
 
 class Player(User):
-    icon = models.ImageField()
+    icon = models.ImageField(null=True, blank=True)
     wins = models.IntegerField(default=0)
     losses = models.IntegerField(default=0) 
 
@@ -30,7 +30,7 @@ class Game(models.Model):
 
 class Feature(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='game')
     size = models.IntegerField(default=0)
     is_completed = models.BooleanField(default=False)
 
@@ -38,36 +38,16 @@ class Feature(models.Model):
         raise NotImplementedError
 
 class Road(Feature):
-    def __init__(self, sz, comp):
-        self.size = sz
-        self.is_completed = comp
-
     def score():
         return size
 
 class City(Feature):
     coats_of_arms = models.IntegerField(default=0)
 
-    def __init(self, sz, comp, cts):
-        self.size = sz
-        self.is_completed = comp
-        self.coats_of_arms = cts
-
     def score():
         factor = 2 if is_completed else 1
         return size * factor + coats * factor
 
 class Monastery(Feature):
-    def __init__(self, sz):
-        # Completion status can be inferred from size
-        if sz > 9 or sz < 1:
-            raise ValueError("The size of a monastery must be at least 1 and no more than 9 tiles.")
-        elif sz == 9:
-            self.size = sz
-            self.is_completed = True
-        else:
-            self.size = sz
-            self.is_completed = False
-
     def score():
         return size
